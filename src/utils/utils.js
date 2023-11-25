faf502051664d8eb916e51fcf0f4ba171da169e0
RizwanSuvo99 <rizwanshuvo@gmail.com>
2023-11-25 11:23:42 +0600

    utils updated

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,8 @@
 export const utils = () => {
   let data = localStorage.getItem("books");
-  if (data) {
-    return JSON.parse(data);
-  }
-  return [];
+  // if (data) {
+  //   return JSON.parse(data);
+  // }
+  // return [];
+  return JSON.parse(data) || [];
 };
